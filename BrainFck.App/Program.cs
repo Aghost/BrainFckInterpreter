57501cc0d339daede88d622c1900d5e4642ed939
Aghost <youri_van_sorgen2@example.com>
2021-10-26 23:18:37 +0200

    initial

---
+++ BrainFck.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BrainFck.Core;
+using static System.Console;
+
+namespace BrainFck.App
+{
+    class Program
+    {
+        static readonly string HelloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+        static void Main(string[] args)
+        {
+            BrainFckInterpreter BFI = args.Length < 1 ? new(HelloWorld) : new(String.Join(" ", args));
+
+            BFI.Run();
+        }
+    }
+
+}
---
+++ BrainFck.Core/BrainFckInterpreter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Console;
+
+namespace BrainFck.Core
+{
+    public class BrainFckInterpreter
+    {
+        public byte[] memory;
+        public int pointer;
+        public char[] input;
+
+        public BrainFckInterpreter(string program) {
+            input = program.ToCharArray();
+            memory = new byte[1000];
+        }
+
+        public void Run() {
+            int brackets = 0;
+
+            for (int i = 0; i < input.Length; i++) {
+                switch(input[i]) {
+                    case '>': pointer++; break;
+                    case '<': pointer--; break;
+                    case '+': memory[pointer]++; break;
+                    case '-': memory[pointer]--; break;
+                    case '.': Write((char)(memory[pointer])); break;
+                    case ',': memory[pointer] = (byte)ReadKey().KeyChar; break;
+                    case '[':
+                              if (memory[pointer] == 0) {
+                                  brackets++;
+                                  while (input[i] != ']' || brackets != 0) {
+                                      i++;
+                                      if (input[i] == '[')
+                                          brackets++;
+                                      else if (input[i] == ']')
+                                          brackets--;
+                                  }
+                              }
+                              break;
+                    case ']':
+                              if (memory[pointer] != 0) {
+                                  brackets++;
+                                  while (input[i] != '[' || brackets != 0) {
+                                      i--;
+                                      if (input[i] == ']')
+                                          brackets++;
+                                      else if (input[i] == '[')
+                                          brackets--;
+                                  }
+                              }
+                              break;
+                }
+            }
+        }
+
+    }
+}
